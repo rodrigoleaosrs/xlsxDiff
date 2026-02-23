1a80e726b758880b4048fdfebdb76aa4726775ce
rodrigoleaosrs <113530561+rodrigoleaosrs@users.noreply.github.com>
2026-02-23 09:31:53 -0300

    Add LSM comparison command with highlighting
    Updated the batch script to include a new comparison command for LSMs with highlighting.

--- test-data/process_demo_spreadsheets.bat
+++ test-data/process_demo_spreadsheets.bat
@@ -1,10 +1,2 @@
-REM Just compare two spreadsheets
-python ..\xlsxDiff.py in1.xlsx in2.xlsx out_demo.xlsx
-REM Just compare two spreadsheets, but use formulas rather than data
-python ..\xlsxDiff.py in1.xlsx in2.xlsx out_demo_formulas.xlsx -f
-REM Compare two spreadsheets and find added/deleted rows/columns
-python ..\xlsxDiff.py in1.xlsx in2.xlsx out_demo_cBr1.xlsx -c books!B -r books!1  -c rmdata!A -r rmdata!1 -c staff!B -r staff!1
-REM Compare two spreadsheets and find added/deleted rows/columns, use extended index
-python ..\xlsxDiff.py in1.xlsx in2.xlsx out_demo_cBCr1.xlsx -c books!B -r books!1  -c rmdata!A -r rmdata!1 -c staff!B,C -r staff!1
-REM Compare two spreadsheets and find added/deleted rows/columns. No special highlighting
-python ..\xlsxDiff.py in1.xlsx in2.xlsx out_demo_cBCr1X.xlsx -c books!B -r books!1  -c rmdata!A -r rmdata!1 -c staff!B,C -r staff!1 -X
+REM Compara 2 LSMs, em relação a coluna C da aba "OS 1", com highlight em verde das colunas e linhas que modificaram 
+python ..\xlsxDiff.py 1-ANTIGA.xlsx 2-NOVA.xlsx 3-ALTERACOES.xlsx -c "OS 1"!C --highlight -a
