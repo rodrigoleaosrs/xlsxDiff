2221ed8dd914fc54a118795a7ea7a26a84b71ce9
Rodrigo Soares <rodrigoleaosrs@gmail.com>
2026-02-23 09:54:02 -0300

    Configurado para comparar 2 LSMs

--- test-data/comparar LSMs.bat
+++
@@ -1,2 +0,0 @@
-REM Compara 2 LSMs, em relação a coluna C da aba "OS 1", com highlight em verde das colunas e linhas que modificaram 
-python ..\xlsxDiff.py 1-ANTIGA.xlsx 2-NOVA.xlsx 3-ALTERACOES.xlsx -c "OS 1"!C --highlight -a
